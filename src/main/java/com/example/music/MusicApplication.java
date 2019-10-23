114881aa065c6066d77ed2478fb6e32d24cdcdac
mahaitao <mahaitao@mahaitaodeMacBook-Pro.local>
2019-10-24 01:46:20 +0800

    add comment

--- src/main/java/com/example/music/MusicApplication.java
+++ src/main/java/com/example/music/MusicApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MusicApplication {
 
-
+    // start from here
     public static void main(String[] args) {
         SpringApplication.run(MusicApplication.class, args);
     }
